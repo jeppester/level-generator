a0698779271bf1ccfcce9887ced4790d623375a8
Jesper Sørensen <jeppester1@gmail.com>
2017-02-05 21:42:37 +0100

    Add VERY simple level creation algorithm

--- index.js
+++ index.js
@@ -6,9 +6,58 @@
     root.LevelGenerator = factory();
   }
 }(this, function () {
-  function LevelGenerator() {
-    console.log('I am a level generator');
-  }
+  return {
+    createLevel: function (tilesCount) {
+      const tiles = [this.createTile(0, 0)];
 
-  return LevelGenerator;
+      while (tiles.length < tilesCount) {
+        this.addRandomTile(tiles);
+      }
+
+      return tiles;
+    },
+
+    addRandomTile: function (tiles) {
+      // Pick a random tile from the list
+      const tile = this.pickTileWithFreeSides(tiles);
+
+      // Add a new tile on one of the sides
+      const position = this.pickFreeSideOfTile(tile, tiles);
+      tiles.push(this.createTile(position.x, position.y));
+    },
+
+    pickTileWithFreeSides: function (tiles) {
+      let tile;
+      do {
+        tile = tiles[Math.floor(Math.random() * tiles.length)];
+      }
+      while (this.getFreeSideTiles(tile, tiles).length === 0);
+      return tile;
+    },
+
+    pickFreeSideOfTile: function (tile, tiles) {
+      const sideTiles = this.getFreeSideTiles(tile, tiles);
+
+      return sideTiles[Math.floor(Math.random() * sideTiles.length)];
+    },
+
+    getFreeSideTiles: function (tile, tiles) {
+      return [
+        this.createTile(tile.x - 1, tile.y),
+        this.createTile(tile.x + 1, tile.y),
+        this.createTile(tile.x, tile.y - 1),
+        this.createTile(tile.x, tile.y - 1),
+      ].filter((tile) => !this.tileByPosition(tile.x, tile.y, tiles))
+    },
+
+    tileByPosition: function (x, y, tiles) {
+      return tiles.find(function (tile) {
+        return tile.x === x && tile.y === y
+      });
+    },
+
+    createTile: function (x, y) {
+      return { x: x, y: y };
+    },
+  };
 }))
